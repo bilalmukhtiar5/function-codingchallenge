7c2ac17f05c8ac0f8555d7c8545f3d38a43f1cda
bilalmukhtiar5 <billuedwardian08@gmail.com>
2023-10-02 14:51:14 +0500

    challenges added

---
+++ script.js
@@ -0,0 +1,81 @@
+/*
+Task: 1 Write a function called sum 
+that takes in two numbers as parameters
+and returns their sum.
+const sum = (a,b) => 
+a+b;
+let total = sum(6,2);
+console.log(total);
+*/
+
+
+// function sum (a,b) {
+//       return a+b;
+// }
+// const num1 = 5;
+// const num2 = 10;
+// const result = sum (num1,num2);
+// console.log(`the sum of ${num1} and ${num2} is ${result}`);
+
+
+// Task 2: Write a function called isEven that takes in 
+// a number as a parameter and returns true if the 
+// number is even and false if it is odd.
+
+// function isEven (number) {
+//       return number % 2 === 0;
+// }
+
+// console.log(isEven(4));
+
+
+
+// Task 3: Write a function called reverseString that
+// takes in a string as a parameter and returns the 
+// string in reverse order.
+
+// function reverseString(inputString) {
+//       // Initialize an empty string to store the reversed result
+//       let reversed = '';
+    
+//       // Iterate through the input string in reverse order
+//       for (let i = inputString.length - 1; i >= 0; i--) {
+//         // Append each character to the reversed string
+//         reversed += inputString[i];
+//       }
+    
+//       // Return the reversed string
+//       return reversed;
+//     }
+    
+//     // Example usage:
+//     const originalString = "Hello, World!";
+//     const reversedString = reverseString(originalString);
+//     console.log(reversedString);
+
+
+
+
+// Task 5: Write a function called convertTemperature 
+// that takes in a temperature in Celsius as 
+// parameter and returns the equivalent temperature in
+// Fahrenheit.
+
+/*
+function convertTemperature(celsius) {
+      // Calculate Fahrenheit from Celsius
+      var fahrenheit = (celsius * 9/5) + 32;
+      
+      return fahrenheit;
+    }
+    
+    // Example usage:
+    var celsiusTemperature = 40; // Replace with your desired temperature in Celsius
+    var fahrenheitTemperature = convertTemperature(celsiusTemperature);
+    console.log(`Temperature in Fahrenheit: ${fahrenheitTemperature}°F`);
+
+    */
+
+
+
+    Task 
